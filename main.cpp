59421320dbfb392cd5734e162f6b5fe9dd5e3e37
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-11-14 16:52:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
-   return 0;
+    int n=0;
+    int a=0;
+    int b=1;
+    int c=0;
+
+
+    cin >> n ;
+
+    for (int i=0; i<n; i++){
+
+        c=a+b;
+
+        if(c<=n){
+
+            cout << c << endl;
+
+        b=a;
+
+        a=c;
+        }
+
+    }
+
+    return 0;
 }
